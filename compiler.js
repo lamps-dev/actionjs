a2b1772cb1de11f6d8623a1f6d879f01562c77ed
lamps-dev <rimit58872@gmail.com>
2024-09-08 17:34:25 +0200

    Create compiler.js

---
+++ compiler.js
@@ -0,0 +1,26 @@
+// compiler.js
+const fs = require('fs');
+const path = require('path');
+
+function compile(inputFile, outputFile) {
+    const input = fs.readFileSync(inputFile, 'utf-8');
+    const output = transform(input);
+    fs.writeFileSync(outputFile, output);
+}
+
+function transform(input) {
+    // Simple transformation example
+    return input
+        .replace(/<action>/g, 'document.createElement("div")')
+        .replace(/<\/action>/g, '');
+}
+
+const inputFile = process.argv[2];
+const outputFile = process.argv[3];
+
+if (!inputFile || !outputFile) {
+    console.error('Usage: node compiler.js <inputFile> <outputFile>');
+    process.exit(1);
+}
+
+compile(inputFile, outputFile);
